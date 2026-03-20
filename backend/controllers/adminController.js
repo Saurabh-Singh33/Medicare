b47ff9e976029ab3017372527caf51908aa2aef4
Saurabh-Singh33 <singhsaurabh43431@gmail.com>
2026-03-20 10:11:25 +0530

    Completed the admin api work

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -6,8 +6,12 @@ const addDoctor = async (req,res) =>{
    try {
     
     const{ name , email , password, speciality , degree,experience , about , fees,address } =req.body
+   const imageFile=req.imageFile
 
+   console.log({ name , email , password, speciality , degree,experience , about , fees,address },imageFile)
    } catch (error) {
      
    }
 }
+
+export {addDoctor}
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
 
 const app = express()
 const port = process.env.PORT || 4000
@@ -12,6 +13,10 @@ connectCloudinary()
 
 app.use(express.json())
 app.use(cors())
+
+//api endpoints
+app.use('/api/admin',adminRouter)
+//localhost:4000/api/admin
 
 app.get('/',(req,res)=>{
   res.send("API is working Great 😎")
