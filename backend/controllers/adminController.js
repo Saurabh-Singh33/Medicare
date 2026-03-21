3966721727a003bf673d5572297e506c42eac64d
Saurabh-Singh33 <singhsaurabh43431@gmail.com>
2026-03-21 12:21:24 +0530

    Resolved error of cloundnary and multer

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,58 +1,85 @@
-import validator from 'validator';
-import bcrypt from 'bcrypt'
-import {v2 as cloudnary} from 'cloudinary';
-import doctorModel from "../models/doctorModel.js"
-// API for Adding doctor 
+import validator from "validator";
+import bcrypt from "bcrypt";
+import { v2 as cloudnary } from "cloudinary";
+import doctorModel from "../models/doctorModel.js";
+// API for Adding doctor
 
 const addDoctor = async (req, res) => {
   try {
+    const {
+      name,
+      email,
+      password,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address,
+    } = req.body;
 
-    const { name, email, password, speciality, degree, experience, about, fees, address } = req.body;
     const imageFile = req.file;
 
-    // validation
-    if (!name || !email || !password || !speciality || !degree || !experience || !address) {
-      return res.json({ success: false, message: "Missing Details" });
+    //checking for all data to doctor
+    if (
+      !name ||
+      !email ||
+      !password ||
+      !speciality ||
+      !degree ||
+      !experience ||
+      !address
+    ) {
+      return res.json({ success: false, message: "missing Details" });
     }
 
+    // Validating email
     if (!validator.isEmail(email)) {
-      return res.json({ success: false, message: "Enter valid email" });
+      return res.json({ success: false, message: "Please ener valid email" });
     }
 
+    //Password validation
     if (password.length < 8) {
-      return res.json({ success: false, message: "Password must be 8 chars" });
+      return res.json({
+        success: false,
+        message: "please enter a strong password",
+      });
     }
 
-    // hashing
+    //hashing doctor password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    // image optional
-    const image = imageFile ? imageFile.filename : "";
+    //upload image to cloudnary
+    let imageUrl = "";
 
+    if (imageFile) {
+      const imageUpload = await cloudnary.uploader.upload(imageFile.path, {
+        resource_type: "image",
+      });
+      imageUrl = imageUpload.secure_url;
+    }
     const doctorData = {
       name,
       email,
+      image: imageUrl,
       password: hashedPassword,
-      image,
       speciality,
       degree,
       experience,
       about,
       fees,
       address: JSON.parse(address),
-      date: Date.now()
+      date: Date.now(),
     };
 
     const newDoctor = new doctorModel(doctorData);
     await newDoctor.save();
-
     res.json({ success: true, message: "Doctor added" });
-
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
   }
 };
 
-export {addDoctor}
+export { addDoctor };
