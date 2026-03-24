703c1f2b8cef46d1ad989c7c2796ffc5ec8fac03
Saurabh-Singh33 <singhsaurabh43431@gmail.com>
2026-03-24 22:37:21 +0530

    Worked on Slot booking

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcrypt'
 import userModel from '../models/userModel.js'
 import jwt from 'jsonwebtoken'
 import { v2 as cloudinary } from 'cloudinary'
+import doctorModel from '../models/doctorModel.js'
 
 // API to register user
 const registerUser = async (req, res) => {
@@ -117,6 +118,41 @@ const updateProfile = async (req, res) => {
     }
 
     res.json({ success: true, message: "Profile Updated" })
+
+  } catch (error) {
+    console.log(error)
+    res.json({ success: false, message: error.message })
+  }
+}
+
+// API to book appointment 
+
+const bookAppointment = async(req,res) =>{
+  try {
+    const {userId , docId , slotDate , slotTime} = req.body
+
+    const docData = await doctorModel.findById(docId).select('-password')
+
+    if (!docData.available) {
+      return res.json({success:false , message :"Doctor not available"})
+    }
+    let slots_booked = docData.slots_booked
+
+    // checking for slots availability
+    if (slots_booked[slotDate]) {
+      if (slots_booked[slotDate].includes(slotTime)) {
+        return res.json({success:false , message:'slots not available'})
+      }else{
+        slots_booked[slotDate].push(slotTime)
+      }
+    }else{
+      slots_booked[slotDate] =[]
+      slots_booked[slotDate].push(slotTime)
+    }
+
+    const userData = await userModel.findById(userId).select('-password')
+
+    delete 
 
   } catch (error) {
     console.log(error)
