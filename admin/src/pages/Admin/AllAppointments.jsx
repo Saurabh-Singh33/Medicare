92db230d93e9d31d960404e16df3c95cb555dad2
Saurabh-Singh33 <singhsaurabh43431@gmail.com>
2026-03-29 12:57:20 +0530

    Worked on some poration of Appointments section

--- admin/src/pages/Admin/AllAppointments.jsx
+++ admin/src/pages/Admin/AllAppointments.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { AdminContext } from '../../context/AdminContext'
 
 const AllAppointments = () => {
+
+  const { aToken, appointments, getAllAppointments } = useContext(AdminContext)
+
+  useEffect(() => {
+    if (aToken) {
+      getAllAppointments()
+    }
+  }, [aToken])
+
   return (
-    <div>
-      Appointment
+    <div className='w-full max-w-6xl m-5'>
+      <p className='mb-3 text-lg font-medium'>All Appointments</p>
+
+      <div className='bg-white border rounded text-sm max-h-[80vh] min-h-[60vh] overflow-y-scroll'>
+
+        <div className='hidden sm:grid grid-cols-[0.5fr_3fr_1fr_3fr_3fr_1fr_1fr] grid-flow-col py-3 px-6 border-b'>
+          <p>#</p>
+          <p>Patient</p>
+          <p>Age</p>
+          <p>Data & Time</p>
+          <p>Doctor</p>
+          <p>Fees</p>
+          <p>Actions</p>
+        </div>
+
+        {appointments.map((item, index) => {
+          return (
+            <div className='flex flex-wrap justify-between max-sm:gap-2 sm:grid sm:grid-cols-[0.5fr_3fr_1fr_3fr_3fr_1fr_1fr] items-center text-gray-500 py-3 px-6 border-b hover:bg-gray-50' key={index}>
+              <p className='max-sm:hidden'>{index + 1}</p>
+              <div className='flex items-center gap-2'>
+                <img className='w-8 rounded-full' src={item.userData.Image} alt="" />  {/*  Fixed: capital I */}
+                <p>{item.userData.name}</p>
+              </div>
+            </div>
+          )
+        })}
+
+      </div>
     </div>
   )
 }
