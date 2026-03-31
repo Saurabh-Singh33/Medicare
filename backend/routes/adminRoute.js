1c84fe1a78d3d6185826cbcd2772abb326e6aa90
Saurabh-Singh33 <singhsaurabh43431@gmail.com>
2026-03-31 16:17:17 +0530

    Working on Admin Dashboard

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addDoctor , allDoctors, loginAdmin,appointmentsAdmin, appointmentCancel } from '../controllers/adminController.js'
+import { addDoctor , allDoctors, loginAdmin,appointmentsAdmin, appointmentCancel,adminDashboard } from '../controllers/adminController.js'
 import upload from '../middlewares/multer.js'
 import authAdmin from '../middlewares/authAdmin.js'
 import { changeAvailabilty } from '../controllers/doctorController.js'
@@ -12,5 +12,6 @@ adminRouter.post('/all-doctors',authAdmin,allDoctors)
 adminRouter.post('/change-availability',authAdmin,changeAvailabilty)
 adminRouter.get('/appointments', authAdmin , appointmentsAdmin)
 adminRouter.post('/cancel-appointment' , authAdmin,appointmentCancel)
+adminRouter.get('/dashboard', authAdmin, adminDashboard)
 
 export default adminRouter
