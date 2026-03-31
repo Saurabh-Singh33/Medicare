b9fdaea3a4b0d4739bdb5bf143ee6afcbe56d2f1
Saurabh-Singh33 <singhsaurabh43431@gmail.com>
2026-04-01 03:55:56 +0530

    completed dashboard section od admin side

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -180,7 +180,7 @@ const adminDashboard = async(req,res)=>{
       doctors:doctors.length,
       appointments:appointments.length,
       patients:users.length,
-      latestAppointments: appointments.reverse.slice(0,5)
+      latestAppointments: appointments.reverse(). slice(0,5)
       
      }
 
