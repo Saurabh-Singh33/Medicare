b3231cb5efa27ac04905e988b5e47a59a423e226
Saurabh-Singh33 <singhsaurabh43431@gmail.com>
2026-04-01 06:27:29 +0530

    Started working on Doctors Section

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -2,14 +2,14 @@ import express from 'express'
 import { addDoctor , allDoctors, loginAdmin,appointmentsAdmin, appointmentCancel,adminDashboard } from '../controllers/adminController.js'
 import upload from '../middlewares/multer.js'
 import authAdmin from '../middlewares/authAdmin.js'
-import { changeAvailabilty } from '../controllers/doctorController.js'
+import { changeAvailability } from '../controllers/doctorController.js'
 
 const adminRouter = express.Router()
 
 adminRouter.post('/add-doctor',authAdmin, upload.single('image'), addDoctor)
 adminRouter.post('/login',loginAdmin)
 adminRouter.post('/all-doctors',authAdmin,allDoctors)
-adminRouter.post('/change-availability',authAdmin,changeAvailabilty)
+adminRouter.post('/change-availability',authAdmin,changeAvailability)
 adminRouter.get('/appointments', authAdmin , appointmentsAdmin)
 adminRouter.post('/cancel-appointment' , authAdmin,appointmentCancel)
 adminRouter.get('/dashboard', authAdmin, adminDashboard)
